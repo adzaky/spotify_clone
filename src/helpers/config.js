eb254b38c37edd149bff7a7c0366cb60d9a20467
zkyzzz <muhadlim17@gmail.com>
2024-02-01 15:08:41 +0800

    fix: Update Spotify Web-API App Client ID

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,4 +1,4 @@
-export const CLIENT_ID = "e1f72915e3814f5286be671f7fe13252";
+export const CLIENT_ID = "9692fe2135424dfb90a5852b291df583";
 export const AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
 export const REDIRECT_URI = "http://localhost:4001/";
 export const SCOPE = ["user-top-read", "user-read-recently-played"];
