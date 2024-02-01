3bb3b738d8079a4833086df62539ec6b6860cfb8
zkyzzz <muhadlim17@gmail.com>
2024-02-01 15:21:09 +0800

    components: Wrapper: Use Ant Design Layout and adjust styling

--- src/components/Wrapper/index.jsx
+++ src/components/Wrapper/index.jsx
@@ -2,18 +2,22 @@ import React from "react";
 import Sidebar from "../Sidebar";
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar";
+import { Layout } from "antd";
+import { Content } from "antd/es/layout/layout";
 
 const Wrapper = () => {
   return (
-    <div className="bg-black p-2">
-      <div className="flex gap-x-2">
-        <Sidebar />
-        <div className="flex-1 h-screen bg-gradient-to-b from-[#5B3838] to-[#272626] rounded-md p-8">
-          <Navbar />
-          <Outlet />
+    <Layout className="bg-black h-screen flex flex-row font-poppins">
+      <div className="flex">
+        <div className="ml-2 my-2">
+          <Sidebar />
         </div>
       </div>
-    </div>
+      <Content className="m-2 bg-gradient-to-b from-[#5B3838] to-[#272626] rounded-md overflow-y-auto p-6 no-scrollbar font-poppins">
+        <Navbar />
+        <Outlet />
+      </Content>
+    </Layout>
   );
 };
 
