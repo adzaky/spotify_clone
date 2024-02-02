08ea3a01743da6c710bae517c43cdf8a2c26826b
zkyzzz <muhadlim17@gmail.com>
2024-02-02 14:41:48 +0800

    refactor: move API endpoints to helpers

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,5 +1,4 @@
 export const CLIENT_ID = "9692fe2135424dfb90a5852b291df583";
-export const AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
 export const REDIRECT_URI = "http://localhost:4001/";
 export const SCOPE = ["user-top-read", "user-read-recently-played"];
 export const SCOPE_PARAM = SCOPE.join("%20")
---
+++ src/helpers/endpoint.js
@@ -0,0 +1,2 @@
+export const AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
+export const BASE_ENDPOINT = "https://api.spotify.com/v1/";
