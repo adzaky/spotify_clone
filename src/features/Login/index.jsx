9202fe55b6db9c5864807834ee9c48f4e35af49f
zkyzzz <muhadlim17@gmail.com>
2024-02-02 14:45:15 +0800

    refactor: Login: Move handleLogin function to a separate hook

--- src/features/Login/index.jsx
+++ src/features/Login/index.jsx
@@ -1,29 +1,9 @@
-import { useEffect, useState } from "react";
-import {
-  AUTHORIZE_ENDPOINT,
-  CLIENT_ID,
-  REDIRECT_URI,
-  SCOPE_PARAM,
-} from "../../helpers/config";
 import { useNavigate } from "react-router-dom";
+import { useLogin } from "../../hooks/useLogin";
 
 const LoginFeatures = () => {
-  const [token, setToken] = useState("");
+  const { token, handleLogin } = useLogin();
   const navigate = useNavigate();
-
-  const handleLogin = () => {
-    window.location.href = `${AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE_PARAM}&response_type=token&show_dialog=true`;
-  };
-
-  useEffect(() => {
-    const params = new URLSearchParams(window.location.hash.substring(1));
-    const access_token = params.get("access_token");
-
-    if (access_token) {
-      localStorage.setItem("access_token", access_token);
-      setToken(access_token);
-    }
-  }, []);
 
   return (
     <>
