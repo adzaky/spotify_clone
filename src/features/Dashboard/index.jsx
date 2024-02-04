5000327a99e15cd5be017f50a336e9bd045a144e
zkyzzz <muhadlim17@gmail.com>
2024-02-04 16:17:33 +0800

    refactor: Dashboard: Display Top Musics instead of Recently Played tracks

--- src/features/Dashboard/index.jsx
+++ src/features/Dashboard/index.jsx
@@ -1,18 +1,14 @@
-import React, { useEffect } from "react";
-import { useRecentlyPlayed } from "../../hooks/useRecentlyPlayed";
+import React from "react";
 import Searchbar from "../../components/Searchbar";
 import { useArtistID } from "../../hooks/useArtistID";
 import { Card, ConfigProvider, theme } from "antd";
 import Meta from "antd/es/card/Meta";
+import { useTopItems } from "../../hooks/useTopItems";
 const { darkAlgorithm } = theme;
 
 const DashboardFeatures = () => {
-  const { data, getRecentlyPlayed } = useRecentlyPlayed();
   const { artist, setSearchInput } = useArtistID();
-
-  useEffect(() => {
-    getRecentlyPlayed();
-  }, []);
+  const { topMusics } = useTopItems("tracks", 6);
 
   return (
     <ConfigProvider
@@ -21,17 +17,19 @@ const DashboardFeatures = () => {
       }}
     >
       <div className="flex md:flex-wrap overflow-x-auto justify-between gap-4 no-scrollbar">
-        {data?.slice(0, 6).map((item, index) => (
+        {topMusics?.map((item, index) => (
           <div
             key={index}
             className="flex shrink-0 gap-4 justify-start items-center w-full md:w-[48%] lg:w-[32%] backdrop-blur-sm hover:bg-black/80 bg-black/60 rounded-md cursor-pointer duration-300"
           >
-            <img
-              alt={item.track?.artists[0]?.name}
-              src={item.track?.album?.images[0]?.url}
-              className="size-20 rounded-l-md"
-            />
-            <h1 className="pr-6">{item.track?.album?.name}</h1>
+            <a href={item?.album?.external_urls?.spotify}>
+              <img
+                alt={item?.name}
+                src={item?.album?.images[0]?.url}
+                className="size-20 rounded-l-md"
+              />
+            </a>
+            <h1 className="pr-6">{item?.album?.name}</h1>
           </div>
         ))}
       </div>
@@ -44,16 +42,21 @@ const DashboardFeatures = () => {
           {artist.tracks?.items?.map((item, index) => {
             return (
               <div key={index} className="w-[48%] md:w-[24%] lg:w-[16%]">
-                <Card
-                  bordered={false}
-                  hoverable
-                  cover={
-                    <img alt={item.name} src={item.album?.images[0]?.url} />
-                  }
-                  className="w-full mt-6"
-                >
-                  <Meta title={item.name} description={item.artists[0]?.name} />
-                </Card>
+                <a href={item?.external_urls?.spotify}>
+                  <Card
+                    bordered={false}
+                    hoverable
+                    cover={
+                      <img alt={item.name} src={item.album?.images[0].url} />
+                    }
+                    className="w-full mt-6"
+                  >
+                    <Meta
+                      title={item.name}
+                      description={item.artists[0].name}
+                    />
+                  </Card>
+                </a>
               </div>
             );
           })}
