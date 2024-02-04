8ca78a2ae465ceed0714425617d84dbedaeae74b
zkyzzz <muhadlim17@gmail.com>
2024-02-04 16:16:34 +0800

    refactor: RecentlyPlayed: Update styling and layouting Cards

--- src/features/RecentlyPlayed/index.jsx
+++ src/features/RecentlyPlayed/index.jsx
@@ -18,25 +18,27 @@ const RecentlyPlayedFeatures = () => {
       }}
     >
       <h1 className="text-2xl">Recently Played</h1>
-      <div className="flex flex-wrap justify-between gap-4 mt-4 overflow-y-auto rounded-md no-scrollbar">
+      <div className="w-full flex flex-wrap justify-between mt-4 overflow-y-auto rounded-md no-scrollbar">
         {data?.map((item, index) => (
-          <div key={index}>
-            <Card
-              bordered={false}
-              hoverable
-              cover={
-                <img
-                  alt={item.track?.artists[0]?.name}
-                  src={item.track?.album?.images[0]?.url}
+          <div key={index} className="w-[48%] lg:w-[18%]">
+            <a href={item?.external_urls?.spotify}>
+              <Card
+                bordered={false}
+                hoverable
+                cover={
+                  <img
+                    alt={item.track?.artists[0].name}
+                    src={item.track?.album?.images[0].url}
+                  />
+                }
+                className="w-full mt-6"
+              >
+                <Meta
+                  title={item.track?.name}
+                  description={item.track?.album?.artists[0].name}
                 />
-              }
-              className="w-56"
-            >
-              <Meta
-                title={item.track?.name}
-                description={item.track?.album?.artists[0]?.name}
-              />
-            </Card>
+              </Card>
+            </a>
           </div>
         ))}
       </div>
