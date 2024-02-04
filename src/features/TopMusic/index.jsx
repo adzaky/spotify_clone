405360edf3f912eaddaa89a015a3cfe61e75d545
zkyzzz <muhadlim17@gmail.com>
2024-02-04 16:14:18 +0800

    feat: TopMusic: Display Top Artist's Tracks for User

--- src/features/TopMusic/index.jsx
+++ src/features/TopMusic/index.jsx
@@ -1,15 +1,48 @@
 import React from "react";
 import MusicsCarousel from "../../components/Carousel/MusicsCarousel";
+import { useTopItems } from "../../hooks/useTopItems";
+import { Card, ConfigProvider, theme } from "antd";
+const { Meta } = Card;
+const { darkAlgorithm } = theme;
 
 const TopMusicFeatures = () => {
+  const { topMusics } = useTopItems("tracks", 20);
+
   return (
     <div className="w-full flex flex-col justify-center items-center">
       <div className="w-full">
         <MusicsCarousel />
       </div>
-      <div className="flex mt-6 bg-black w-full">
-        <h1>Placeholder Content</h1>
-      </div>
+      <ConfigProvider
+        theme={{
+          algorithm: darkAlgorithm,
+        }}
+      >
+        <h1 className="text-2xl w-full mt-6">Top Tracks For You</h1>
+        <div className="flex w-full">
+          <div className="flex flex-wrap justify-between overflow-y-auto rounded-md no-scrollbar">
+            {topMusics?.map((item, index) => (
+              <div key={index} className="w-[48%] lg:w-[18%]">
+                <a href={item?.external_urls?.spotify}>
+                  <Card
+                    bordered={false}
+                    hoverable
+                    cover={
+                      <img alt="Tracks" src={item?.album?.images[0]?.url} />
+                    }
+                    className="w-full mt-6"
+                  >
+                    <Meta
+                      title={item?.name}
+                      description={item?.artists[0]?.name}
+                    />
+                  </Card>
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+      </ConfigProvider>
     </div>
   );
 };
