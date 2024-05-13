5d33c793c956f7e60564fc9edf70138383a51fb7
adzaky <muhadlim17@gmail.com>
2024-05-14 00:25:31 +0800

    feat: update config client id

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,4 +1,4 @@
-export const CLIENT_ID = "9692fe2135424dfb90a5852b291df583";
+export const CLIENT_ID = "d524b5e56c154c1eac427a468ba571dd";
 export const REDIRECT_URI = "https://songfy-opal.vercel.app/";
 export const SCOPE = ["user-top-read", "user-read-recently-played"];
 export const SCOPE_PARAM = SCOPE.join("%20")
