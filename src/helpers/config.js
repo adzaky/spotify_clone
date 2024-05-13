0f9804e2b95b9b525b314dfb2ca468a1234e83c7
adzaky <muhadlim17@gmail.com>
2024-05-14 00:16:29 +0800

    feat: update config redirect uri

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,4 +1,4 @@
 export const CLIENT_ID = "9692fe2135424dfb90a5852b291df583";
-export const REDIRECT_URI = "http://localhost:4001/";
+export const REDIRECT_URI = "https://songfy-opal.vercel.app/";
 export const SCOPE = ["user-top-read", "user-read-recently-played"];
 export const SCOPE_PARAM = SCOPE.join("%20")
